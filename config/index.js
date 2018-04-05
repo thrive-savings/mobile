2d6ba1cfe4100935e7ff4c8244fe67e8ba10f4b9
nbaghiro <naib.baghirov@gmail.com>
2018-04-05 12:23:31 -0400

    Caching app images on load and loading custom fonts

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  API: "https://6e21342f.ngrok.io",
+  API: "https://09bbf597.ngrok.io",
   URL: "https://join.thrivesavings.com",
   FlinksURL: "https://thrive-iframe.flinks.io"
 }
--- src/boot/setup.js
+++ src/boot/setup.js
@@ -1,13 +1,21 @@
-import * as Expo from "expo";
+import { AppLoading, Asset, Font } from "expo";
 import React, { Component } from "react";
+import { Image } from "react-native";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import { StyleProvider } from "native-base";
 
 import App from "../App";
 import configureStore from "./configureStore";
-import getTheme from "../theme/components";
-import variables from "../theme/variables/commonColor";
+
+function cacheImages(images) {
+  return images.map(image => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+}
 
 export default class Setup extends Component {
   state: {
@@ -15,6 +23,7 @@ export default class Setup extends Component {
     persistor: Object,
     isReady: boolean
   };
+
   constructor() {
     super();
     const { store, persistor } = configureStore();
@@ -24,31 +33,43 @@ export default class Setup extends Component {
       isReady: false
     };
   }
-  componentWillMount() {
-    this.loadFonts();
-  }
-  async loadFonts() {
-    await Expo.Font.loadAsync({
+
+  async _loadAssetsAsync() {
+    const imageAssets = cacheImages([
+      require("../../assets/Backgrounds/BackgroundAccount.png"),
+      require("../../assets/Backgrounds/BackgroundFull.png"),
+      require("../../assets/WorkTypes/Contract.png"),
+      require("../../assets/WorkTypes/Fulltime.png"),
+      require("../../assets/WorkTypes/Parttime.png"),
+      require("../../assets/WorkTypes/Sharing.png"),
+    ]);
+    await Promise.all([...imageAssets]);
+
+    await Font.loadAsync({
+      LatoRegular: require("../../assets/fonts/Lato/Lato-Regular.ttf"),
+      LatoBold: require("../../assets/fonts/Lato/Lato-Bold.ttf"),
+      LatoItalic: require("../../assets/fonts/Lato/Lato-Italic.ttf"),
       Roboto: require("native-base/Fonts/Roboto.ttf"),
       Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
       Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf")
     });
-
-    this.setState({ isReady: true });
   }
 
   render() {
-    if (!this.state.isReady || this.state.isLoading) {
-      return <Expo.AppLoading />;
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this._loadAssetsAsync}
+          onFinish={() => this.setState({ isReady: true })}
+        />
+      );
     }
     return (
-      <StyleProvider style={getTheme(variables)}>
-        <Provider store={this.state.store}>
-          <PersistGate loading={<Expo.AppLoading />} persistor={this.state.persistor}>
-            <App />
-          </PersistGate>
-        </Provider>
-      </StyleProvider>
+      <Provider store={this.state.store}>
+        <PersistGate loading={<AppLoading />} persistor={this.state.persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
     );
   }
 }
