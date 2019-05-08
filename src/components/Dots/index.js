a792baa257d814a964d8c3235d919261c04a835b
nbaghiro <naib.baghirov@gmail.com>
2019-05-08 14:04:37 -0400

    generic Dots component

--- src/components/Dots/index.js
+++ src/components/Dots/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { Svg } from "expo";
 import { View } from "native-base";
@@ -7,69 +7,50 @@ import styles from "./styles";
 
 const colors = require("../../theme/colors");
 
-class Dots extends Component {
-  renderTwo() {
-    return (
-      <Svg width={24} height={10}>
-        <Svg.Circle cx="4" cy="4" r={3} stokeWidth={1} stroke={colors.blue} fill={this.props.step === 1 ? colors.blue : "white"} />
-        <Svg.Circle cx="20" cy="4" r={3} stokeWidth={1} stroke={colors.blue} fill={this.props.step === 2 ? colors.blue : "white"} />
+const STROKE_WIDTH = 1;
+const RADIUS = 4;
+const DISTANCE_BETWEEN = 4;
+const DISTANCE_BETWEEN_ORIGINS = 4 + 2 * RADIUS;
+const containerWidth = n => 2 * n * RADIUS + (n - 1) * DISTANCE_BETWEEN;
+
+function Dots({ count, step, strokeColor, fillColor, emptyColor }) {
+  const circles = [];
+  for (let i = 0; i < count; i++) {
+    circles.push(
+      <Svg.Circle
+        key={`dot_${i}`}
+        cx={RADIUS + i * DISTANCE_BETWEEN_ORIGINS}
+        cy={RADIUS}
+        r={RADIUS - STROKE_WIDTH}
+        stokeWidth={STROKE_WIDTH}
+        stroke={strokeColor}
+        fill={step === i + 1 ? fillColor : emptyColor}
+      />
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <Svg width={containerWidth(count)} height={2 * RADIUS}>
+        {circles}
       </Svg>
-    );
-  }
-
-  renderThree() {
-    return (
-      <Svg width={40} height={10}>
-        <Svg.Circle cx="4" cy="4" r={3} stokeWidth={1} stroke={colors.blue} fill={this.props.step === 1 ? colors.blue : "white"} />
-        <Svg.Circle cx="20" cy="4" r={3} stokeWidth={1} stroke={colors.blue} fill={this.props.step === 2 ? colors.blue : "white"} />
-        <Svg.Circle cx="36" cy="4" r={3} stokeWidth={1} stroke={colors.blue} fill={this.props.step === 3 ? colors.blue : "white"} />
-      </Svg>
-    );
-  }
-
-  renderFour() {
-    return (
-      <Svg width={56} height={10}>
-        <Svg.Circle cx="4" cy="4" r={3} stokeWidth={1} stroke={colors.blue} fill={this.props.step === 1 ? colors.blue : "white"} />
-        <Svg.Circle cx="20" cy="4" r={3} stokeWidth={1} stroke={colors.blue} fill={this.props.step === 2 ? colors.blue : "white"} />
-        <Svg.Circle cx="36" cy="4" r={3} stokeWidth={1} stroke={colors.blue} fill={this.props.step === 3 ? colors.blue : "white"} />
-        <Svg.Circle cx="52" cy="4" r={3} stokeWidth={1} stroke={colors.blue} fill={this.props.step === 4 ? colors.blue : "white"} />
-      </Svg>
-    );
-  }
-
-  render() {
-    let body;
-    switch (this.props.count) {
-      case 2:
-        body = this.renderTwo();
-        break;
-      case 3:
-        body = this.renderThree();
-        break;
-      case 4:
-        body = this.renderFour();
-        break;
-      default:
-        body = this.renderFour();
-        break;
-    }
-
-    return (
-      <View style={styles.container}>
-        {body}
-      </View>
-    );
-  }
+    </View>
+  );
 }
 
 Dots.propTypes = {
   step: PropTypes.number,
-  count: PropTypes.number
+  count: PropTypes.number,
+  strokeColor: PropTypes.string,
+  fillColor: PropTypes.string,
+  emptyColor: PropTypes.string
 };
 Dots.defaultProps = {
   step: 1,
-  count: 4
+  count: 4,
+  strokeColor: colors.blue,
+  fillColor: colors.blue,
+  emptyColor: "white"
 };
 
 export default Dots;
